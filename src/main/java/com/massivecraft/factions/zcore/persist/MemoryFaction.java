bd7b57cf5ad9d693b8dc26e790143b6c351718c8
Naman <ngupta@irepertoire.com>
2018-07-11 09:03:47 -0500

    Fixed package path for InventoryUtil

--- src/main/java/com/massivecraft/factions/zcore/persist/MemoryFaction.java
+++ src/main/java/com/massivecraft/factions/zcore/persist/MemoryFaction.java
@@ -23,6 +23,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
