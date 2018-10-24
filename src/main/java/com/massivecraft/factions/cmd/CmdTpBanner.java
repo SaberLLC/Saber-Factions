ffc4812be9ec58a19c5480bd85b93f708b26f664
ProSavage <naman0311@gmail.com>
2018-10-24 10:48:08 -0500

    Edited tpBanner to work correctly

--- src/main/java/com/massivecraft/factions/cmd/CmdTpBanner.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdTpBanner.java
@@ -1,6 +1,7 @@
 package com.massivecraft.factions.cmd;
 
 import com.massivecraft.factions.SavageFactions;
+import com.massivecraft.factions.listeners.FactionsBlockListener;
 import com.massivecraft.factions.listeners.FactionsPlayerListener;
 import com.massivecraft.factions.struct.Permission;
 import com.massivecraft.factions.util.WarmUpUtil;
@@ -31,12 +32,12 @@ public class CmdTpBanner extends FCommand {
 
       final FactionsPlayerListener fpl = new FactionsPlayerListener(SavageFactions.plugin);
 
-        if (FactionsPlayerListener.bannerLocations.containsKey(fme.getTag())) {
+      if (FactionsBlockListener.bannerLocations.containsKey(fme.getTag())) {
             fme.msg(TL.COMMAND_TPBANNER_SUCCESS);
             this.doWarmUp(WarmUpUtil.Warmup.BANNER, TL.WARMUPS_NOTIFY_TELEPORT, "Banner", new Runnable() {
                 @Override
                 public void run() {
-                    me.teleport(FactionsPlayerListener.bannerLocations.get(fme.getTag()));
+                  me.teleport(FactionsBlockListener.bannerLocations.get(fme.getTag()));
                 }
             }, this.p.getConfig().getLong("warmups.f-banner", 0));
         } else {
