2bc80a8383e39c6e410e3841bf02ad6eb46bdbeb
ProSavage <naman0311@gmail.com>
2018-11-02 14:11:40 -0500

    Fixed issue with faction names not being able to have the letter 'p' in it. (lowercase)

--- src/main/java/com/massivecraft/factions/util/MiscUtil.java
+++ src/main/java/com/massivecraft/factions/util/MiscUtil.java
@@ -20,7 +20,7 @@ public class MiscUtil {
 
     /// TODO create tag whitelist!!
     public static HashSet<String> substanceChars =
-            new HashSet<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "plugin", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
+            new HashSet<>(Arrays.asList("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
 
     public static EntityType creatureTypeFromEntity(Entity entity) {
         if (!(entity instanceof Creature)) {
