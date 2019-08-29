8c656eea6e9f36a8749acf0ec2ab5ac3953ec179
MTM123 <matiss234@gmail.com>
2019-08-29 15:04:51 +0300

    Implement event for role changes

--- src/main/java/com/massivecraft/factions/zcore/persist/MemoryFPlayer.java
+++ src/main/java/com/massivecraft/factions/zcore/persist/MemoryFPlayer.java
@@ -241,7 +241,18 @@ public abstract class MemoryFPlayer implements FPlayer {
         return this.role;
     }
 
-    public void setRole(Role role) { this.role = role; }
+    public void setRole(Role role) {
+        if (this.role == role) {
+            return;
+        }
+
+        FPlayerRoleChangeEvent event = new FPlayerRoleChangeEvent(getFaction(), this, role);
+        Bukkit.getPluginManager().callEvent(event);
+
+        if (!event.isCancelled()) {
+            this.role = event.getTo();
+        }
+    }
 
     public double getPowerBoost() {
         return this.powerBoost;
