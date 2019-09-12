ba86f289944e967aa0c3898aa15d4205f3ea1930
AigleDev <eaglezledozo@gmail.com>
2019-09-12 21:20:20 +0200

    NPE fix

--- src/main/java/com/massivecraft/factions/listeners/FactionsPlayerListener.java
+++ src/main/java/com/massivecraft/factions/listeners/FactionsPlayerListener.java
@@ -804,7 +804,7 @@ public class FactionsPlayerListener implements Listener {
         Block block = event.getClickedBlock();
         Player player = event.getPlayer();
         // Check if the material is bypassing protection
-        if (block == null) return;  // clicked in air, apparently
+        if (block == null || event.getItem() == null) return;  // clicked in air, apparently
         if (Conf.territoryBypassProtectedMaterials.contains(event.getItem().getType())) return;
         if (GetPermissionFromUsableBlock(event.getClickedBlock().getType()) != null) {
             if (!canPlayerUseBlock(player, block, false)) {
