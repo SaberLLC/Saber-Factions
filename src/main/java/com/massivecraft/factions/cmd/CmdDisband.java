541e1fc69bb6bdcb796403180f3ba20b8152b5f7
BrowkS <stefanpopescu923@gmail.com>
2020-05-07 22:36:39 +0300

    Fixed issue - ignoring "useDisbandGui" if the player is not an OP

--- src/main/java/com/massivecraft/factions/cmd/CmdDisband.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdDisband.java
@@ -82,7 +82,7 @@ public class CmdDisband extends FCommand {
         }
 
         if (!access) {
-            if (Conf.useDisbandGUI && !context.fPlayer.isAdminBypassing() || !context.player.isOp()) {
+            if (Conf.useDisbandGUI && (!context.fPlayer.isAdminBypassing() || !context.player.isOp())) {
                 if (!disbandMap.containsKey(context.player.getUniqueId().toString())) {
                     new FDisbandFrame(context.faction).buildGUI(context.fPlayer);
                     return;
