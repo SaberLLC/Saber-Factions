ae262bb97277d2c6347ef78dcab041f70cf46468
Driftay <driftayllc@gmail.com>
2020-12-05 05:33:30 -0500

    Added "faction block broken at player" Skript Expression (BETA)

--- src/main/java/com/massivecraft/factions/skript/expressions/player/PlayerFactionBlockBrokenAt.java
+++ src/main/java/com/massivecraft/factions/skript/expressions/player/PlayerFactionBlockBrokenAt.java
@@ -6,19 +6,26 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
-import com.massivecraft.factions.*;
+import com.massivecraft.factions.Board;
+import com.massivecraft.factions.FLocation;
+import com.massivecraft.factions.Faction;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.BlockBreakEvent;
 
-public class PlayerChunkLocationExpression extends SimpleExpression<String> {
+/**
+ * SaberFactionsX - Developed by Driftay.
+ * All rights reserved 2020.
+ * Creation Date: 12/5/2020
+ */
+public class PlayerFactionBlockBrokenAt extends SimpleExpression<String> {
 
     /**
      * @author Driftay
      */
 
     static {
-        Skript.registerExpression(PlayerChunkLocationExpression.class, String.class, ExpressionType.SIMPLE, "[the] faction chunk at %player%", "[the] %player%['s] chunk");
+        Skript.registerExpression(PlayerFactionBlockBrokenAt.class, String.class, ExpressionType.SIMPLE, "[the] faction block broken at %player%");
     }
 
     Expression<Player> playerExpression;
@@ -40,18 +47,18 @@ public class PlayerChunkLocationExpression extends SimpleExpression<String> {
 
     @Override
     public String toString(Event event, boolean debug) {
-        return "Player Location Expression with expression faction" + playerExpression.toString(event, debug);
+        return "Player Block Broken Within Faction Expression" + playerExpression.toString(event, debug);
     }
 
     @Override
     protected String[] get(Event event) {
         Player player = playerExpression.getSingle(event);
-
-        if (player != null) {
-            FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
-            FLocation floc = new FLocation(fPlayer.getPlayer().getLocation());
-            Faction fac = Board.getInstance().getFactionAt(floc);
-            return new String[]{String.valueOf(fac.getTag())};
+        if (event instanceof BlockBreakEvent) {
+            BlockBreakEvent blockBreakEvent = (BlockBreakEvent) event;
+            if (player != null) {
+                Faction fac = Board.getInstance().getFactionAt(new FLocation(blockBreakEvent.getBlock().getLocation()));
+                return new String[]{String.valueOf(fac.getTag())};
+            }
         }
         return null;
     }
