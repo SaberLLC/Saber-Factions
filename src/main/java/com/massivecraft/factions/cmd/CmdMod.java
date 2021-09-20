b63bc66e369f2101251b3beb0a5e1f5c98a1d8dd
Driftay <driftayllc@gmail.com>
2021-09-19 21:27:25 -0400

    Just remove the async tasks from relational commands all together

--- src/main/java/com/massivecraft/factions/cmd/CmdMod.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdMod.java
@@ -31,59 +31,57 @@ public class CmdMod extends FCommand {
 
     @Override
     public void perform(CommandContext context) {
-        FactionsPlugin.getInstance().getServer().getScheduler().runTaskAsynchronously(FactionsPlugin.instance, () -> {
-            FPlayer you = context.argAsBestFPlayerMatch(0);
-            if (you == null) {
-                FancyMessage msg = new FancyMessage(TL.COMMAND_MOD_CANDIDATES.toString()).color(ChatColor.GOLD);
-                for (FPlayer player : context.faction.getFPlayersWhereRole(Role.NORMAL)) {
-                    String s = player.getName();
-                    msg.then(s + " ").color(ChatColor.WHITE).tooltip(TL.COMMAND_MOD_CLICKTOPROMOTE + s).command("/" + Conf.baseCommandAliases.get(0) + " mod " + s);
-                }
-
-                context.sendFancyMessage(msg);
-                return;
+        FPlayer you = context.argAsBestFPlayerMatch(0);
+        if (you == null) {
+            FancyMessage msg = new FancyMessage(TL.COMMAND_MOD_CANDIDATES.toString()).color(ChatColor.GOLD);
+            for (FPlayer player : context.faction.getFPlayersWhereRole(Role.NORMAL)) {
+                String s = player.getName();
+                msg.then(s + " ").color(ChatColor.WHITE).tooltip(TL.COMMAND_MOD_CLICKTOPROMOTE + s).command("/" + Conf.baseCommandAliases.get(0) + " mod " + s);
             }
 
-            boolean permAny = Permission.MOD_ANY.has(context.sender, false);
-            Faction targetFaction = you.getFaction();
-            if (targetFaction != context.faction && !permAny) {
-                context.msg(TL.COMMAND_MOD_NOTMEMBER, you.describeTo(context.fPlayer, true));
-                return;
-            }
+            context.sendFancyMessage(msg);
+            return;
+        }
 
-            if (you.isAlt()) {
-                return;
-            }
+        boolean permAny = Permission.MOD_ANY.has(context.sender, false);
+        Faction targetFaction = you.getFaction();
+        if (targetFaction != context.faction && !permAny) {
+            context.msg(TL.COMMAND_MOD_NOTMEMBER, you.describeTo(context.fPlayer, true));
+            return;
+        }
 
-            if (context.fPlayer != null && !context.fPlayer.getRole().isAtLeast(Role.COLEADER) && !permAny) {
-                context.msg(TL.COMMAND_MOD_NOTADMIN);
-                return;
-            }
+        if (you.isAlt()) {
+            return;
+        }
 
-            if (you == context.fPlayer && !permAny) {
-                context.msg(TL.COMMAND_MOD_SELF);
-                return;
-            }
+        if (context.fPlayer != null && !context.fPlayer.getRole().isAtLeast(Role.COLEADER) && !permAny) {
+            context.msg(TL.COMMAND_MOD_NOTADMIN);
+            return;
+        }
 
-            if (you.getRole() == Role.LEADER) {
-                context.msg(TL.COMMAND_MOD_TARGETISADMIN);
-                return;
-            }
+        if (you == context.fPlayer && !permAny) {
+            context.msg(TL.COMMAND_MOD_SELF);
+            return;
+        }
 
-            if (you.getRole() == Role.MODERATOR) {
-                // Revoke
-                setRole(you, Role.NORMAL);
-                targetFaction.msg(TL.COMMAND_MOD_REVOKED, you.describeTo(targetFaction, true));
-                context.msg(TL.COMMAND_MOD_REVOKES, you.describeTo(context.fPlayer, true));
-            } else {
-                // Give
-                setRole(you, Role.MODERATOR);
-                targetFaction.msg(TL.COMMAND_MOD_PROMOTED, you.describeTo(targetFaction, true));
-                context.msg(TL.COMMAND_MOD_PROMOTES, you.describeTo(context.fPlayer, true));
-                FactionsPlugin.instance.getFlogManager().log(targetFaction, FLogType.RANK_EDIT, context.fPlayer.getName(), you.getName(), ChatColor.LIGHT_PURPLE + "Mod");
+        if (you.getRole() == Role.LEADER) {
+            context.msg(TL.COMMAND_MOD_TARGETISADMIN);
+            return;
+        }
 
-            }
-        });
+        if (you.getRole() == Role.MODERATOR) {
+            // Revoke
+            setRole(you, Role.NORMAL);
+            targetFaction.msg(TL.COMMAND_MOD_REVOKED, you.describeTo(targetFaction, true));
+            context.msg(TL.COMMAND_MOD_REVOKES, you.describeTo(context.fPlayer, true));
+        } else {
+            // Give
+            setRole(you, Role.MODERATOR);
+            targetFaction.msg(TL.COMMAND_MOD_PROMOTED, you.describeTo(targetFaction, true));
+            context.msg(TL.COMMAND_MOD_PROMOTES, you.describeTo(context.fPlayer, true));
+            FactionsPlugin.instance.getFlogManager().log(targetFaction, FLogType.RANK_EDIT, context.fPlayer.getName(), you.getName(), ChatColor.LIGHT_PURPLE + "Mod");
+
+        }
     }
 
     private void setRole(FPlayer fp, Role r) {
