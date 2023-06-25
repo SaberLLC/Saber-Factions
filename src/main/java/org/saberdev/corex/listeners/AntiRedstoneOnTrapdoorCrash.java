ca5de39b994a11ee4848c3ddb37e055927dae495
Driftay <31221930+Driftay@users.noreply.github.com>
2023-06-25 01:03:24 -0400

    Optimized Anti Redstone TrapDoor

--- src/main/java/org/saberdev/corex/listeners/AntiRedstoneOnTrapdoorCrash.java
+++ src/main/java/org/saberdev/corex/listeners/AntiRedstoneOnTrapdoorCrash.java
@@ -11,56 +11,49 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class AntiRedstoneOnTrapdoorCrash implements Listener {
 
-    private final HashMap<Location, Long> cooldowns = new HashMap<>();
-    private final HashMap<Location, Integer> trapdoorPoweredByRedstoneCounts = new HashMap<>();
+    private final Map<Location, Long> cooldowns = new HashMap<>();
+    private final Map<Location, Integer> trapdoorPoweredByRedstoneCounts = new HashMap<>();
 
     public AntiRedstoneOnTrapdoorCrash() {
         FactionsPlugin plugin = FactionsPlugin.getInstance();
         plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> {
-            // Prevent memory leak
-            if (!trapdoorPoweredByRedstoneCounts.isEmpty()) trapdoorPoweredByRedstoneCounts.clear();
-            if (!cooldowns.isEmpty()) cooldowns.clear();
+            cooldowns.clear();
+            trapdoorPoweredByRedstoneCounts.clear();
         }, 6000L, 6000L);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     private void onRedstonePowerTrapdoor(BlockRedstoneEvent event) {
         Block poweredBlock = event.getBlock();
-        if (!poweredBlock.getType().name().contains("TRAPDOOR")) return;
-        Block blockAbove = poweredBlock.getRelative(BlockFace.UP);
-        if (!blockAbove.getType().name().equals("REDSTONE_WIRE") && !blockAbove.getType().name().equals("REDSTONE")) return;
-
-        final Location trapdoorLoc = poweredBlock.getLocation();
-        final long currentTime = System.currentTimeMillis();
-
-        if (
-                !trapdoorPoweredByRedstoneCounts.containsKey(trapdoorLoc)
-                || !cooldowns.containsKey(trapdoorLoc)
-        ) {
-            trapdoorPoweredByRedstoneCounts.put(trapdoorLoc, 1);
-            cooldowns.put(trapdoorLoc, currentTime);
+        if (!poweredBlock.getType().name().contains("TRAPDOOR")) {
             return;
         }
 
-        int trapdoorOpenByRedstoneCount = trapdoorPoweredByRedstoneCounts.get(trapdoorLoc);
-
-        if (trapdoorOpenByRedstoneCount >= 20) {
-            if (currentTime - cooldowns.get(trapdoorLoc) < 3000) {
-                blockAbove.breakNaturally();
-                poweredBlock.breakNaturally();
-                Logger.print("Prevented possible crash using trapdoors and redstone at: " + trapdoorLoc);
-                return;
-            }
-
-            trapdoorOpenByRedstoneCount = 1;
+        Block blockAbove = poweredBlock.getRelative(BlockFace.UP);
+        if (!blockAbove.getType().name().equals("REDSTONE_WIRE") && !blockAbove.getType().name().equals("REDSTONE")) {
+            return;
         }
 
-        trapdoorOpenByRedstoneCount++;
+        Location trapdoorLoc = poweredBlock.getLocation();
+        long currentTime = System.currentTimeMillis();
 
-        trapdoorPoweredByRedstoneCounts.put(trapdoorLoc, trapdoorOpenByRedstoneCount);
+        trapdoorPoweredByRedstoneCounts.putIfAbsent(trapdoorLoc, 1);
+        cooldowns.putIfAbsent(trapdoorLoc, currentTime);
+
+        int trapdoorOpenByRedstoneCount = trapdoorPoweredByRedstoneCounts.get(trapdoorLoc);
+
+        if (trapdoorOpenByRedstoneCount >= 20 && currentTime - cooldowns.get(trapdoorLoc) < 3000) {
+            blockAbove.breakNaturally();
+            poweredBlock.breakNaturally();
+            Logger.print("Prevented possible crash using trapdoors and redstone at: " + trapdoorLoc);
+            return;
+        }
+
+        trapdoorPoweredByRedstoneCounts.put(trapdoorLoc, trapdoorOpenByRedstoneCount + 1);
         cooldowns.put(trapdoorLoc, currentTime);
     }
 }
